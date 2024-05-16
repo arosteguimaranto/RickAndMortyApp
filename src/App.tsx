d8b12497c1b4b2c2fb97f13471c83c5e9385b831
arosteguimaranto <arosteguimaranto@outlook.com>
2024-05-15 18:49:23 -0600

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Home } from './components/Home';
+import { Locations } from './components/Locations';
+import { Favorites } from './components/Favorites';
+import { Login } from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" Component={Home} />
+        <Route path="/locations" Component={Locations} />
+        <Route path="/favorites" Component={Favorites} />
+        <Route path="/login" Component={Login} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
