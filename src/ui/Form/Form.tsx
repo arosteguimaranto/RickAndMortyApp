4fd8115b3b5a3a7e48c8ace3034d6b1b25ac5186
arosteguimaranto <arosteguimaranto@outlook.com>
2024-05-16 19:00:19 -0600

    feat: styles

--- src/ui/Form/Form.tsx
+++ src/ui/Form/Form.tsx
@@ -3,29 +3,20 @@ import './style.css';
 
 const Form = () => {
   return (
-    <form className="form-container">
-      <div className="form-group">
-        <label htmlFor="email" className="form-label">Correo electrónico:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="form-input"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password" className="form-label">Contraseña:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="form-input"
-          required
-        />
-      </div>
-      <button type="submit" className="form-button">Iniciar Sesión</button>
-    </form>
+		<div className="contenedor">
+		<div className="formulario">
+			<h1>Iniciar Sesion</h1>
+			<form>
+				<label htmlFor="email">Correo electrónico:</label>
+				<input type="email" id="email" name="email" placeholder="Introduce tu correo electrónico" />
+				<label htmlFor="password">Contraseña:</label>
+				<input type="password" id="password" name="password" placeholder="Introduce tu contraseña" />
+				<label htmlFor="remember_me">Recordarme</label>
+				<input type="checkbox" id="remember_me" name="remember_me" />
+				<button className='button-form' type="submit">Iniciar sesión</button>
+			</form>
+		</div>
+	</div>
   );
 };
 
